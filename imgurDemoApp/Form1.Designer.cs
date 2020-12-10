7d55cfc624aecc9e631b5e5fd3733af2325e8456
ray2000719 <ray2000719v414307@users.noreply.github.com>
2020-12-10 20:38:30 +0800

    加入專案檔案。

---
+++ imgurDemoApp/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace imgurDemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_go = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_go
+            // 
+            this.btn_go.Font = new System.Drawing.Font("新細明體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_go.Location = new System.Drawing.Point(276, 27);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(241, 102);
+            this.btn_go.TabIndex = 0;
+            this.btn_go.Text = "GO";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.Btn_go_Click);
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(276, 171);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(241, 213);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 1;
+            this.pb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.btn_go);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.PictureBox pb;
+    }
+}
+
---
+++ imgurDemoApp/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace imgurDemoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class resp
+        {
+            public List<img> data { get; set; }
+        }
+
+        public class img
+        {
+            public string link { get; set; }
+        }
+
+        private resp GetImages(string albumHash, string clientId)
+        {
+            resp result = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://api.imgur.com/3/album/{albumHash}/images");
+
+                //ADD Header
+                WebHeaderCollection myWebHeaderCollection = request.Headers;
+                myWebHeaderCollection.Add("Authorization", $"Client-ID {clientId}");
+
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                string json = readStream.ReadToEnd();
+
+                result = JsonConvert.DeserializeObject<resp>(json);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        private Image GetImageFromUrl(string url)
+        {
+            Image result;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                result = System.Drawing.Image.FromStream(receiveStream);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        private void Btn_go_Click(object sender, EventArgs e)
+        {
+            //抓API list 出有多少東西
+            var imgurData = GetImages("tzZDWAD", "4f37f9a94479861");
+
+            if (imgurData == null)
+            {
+                return;
+            }
+
+            //download 一張照片下來
+            Image image = GetImageFromUrl(imgurData.data[0].link);
+
+            if (image == null)
+            {
+                return;
+            }
+
+            pb.Image = image;
+
+            //json --> object
+            Console.WriteLine(imgurData.data.Count);
+
+            
+
+
+        }
+    }
+}
